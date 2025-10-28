a8d92f65f7175b4f2827826e897840cdc8cc117d
bozak <166830959+bxzxk@users.noreply.github.com>
2025-10-27 22:24:35 -0700

    Update and rename bsbossjump2.lua to bsbossjump.lua

---
+++ bsbossjump.lua
@@ -0,0 +1,38 @@
+task.wait(5.5)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bxzxk/Rblx/refs/heads/main/bsbossjumpbase.lua"))()
+
+task.delay(300, function()
+    local HttpService = game:GetService("HttpService")
+    local TeleportService = game:GetService("TeleportService")
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local placeId = game.PlaceId
+    local cursor
+    local foundServer = nil
+    repeat
+        local url = "https://games.roblox.com/v1/games/"..placeId.."/servers/Public?sortOrder=Asc&limit=100"
+        if cursor then
+            url = url .. "&cursor=" .. cursor
+        end
+        local success, result = pcall(function()
+            return HttpService:JSONDecode(game:HttpGet(url))
+        end)
+        if success and result and result.data then
+            for _, server in ipairs(result.data) do
+                if server.playing < server.maxPlayers and server.id ~= game.JobId then
+                    foundServer = server
+                    break
+                end
+            end
+            cursor = result.nextPageCursor
+        else
+            break
+        end
+    until foundServer or not cursor
+    if foundServer and LocalPlayer then
+        TeleportService:TeleportToPlaceInstance(placeId, foundServer.id, LocalPlayer)
+    else
+        TeleportService:Teleport(placeId, LocalPlayer)
+    end
+end)
