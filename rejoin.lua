84560ac7f28d21fb807dc3727341bb4cec17315a
bozak <166830959+bxzxk@users.noreply.github.com>
2025-11-03 20:35:39 -0800

    rejoin.lua

--- rejoin.lua
+++ rejoin.lua
@@ -1,31 +1,25 @@
 local TeleportService = game:GetService("TeleportService")
 local GuiService = game:GetService("GuiService")
 
--- Automatically grab the place you're currently in
 local placeId = game.PlaceId  
 
--- Function to try reconnecting until successful
 local function reconnect()
     while true do
-        -- small delay so it doesn’t spam requests
         task.wait(15)
-
-        -- try teleporting back
         local success, err = pcall(function()
             TeleportService:Teleport(placeId)
         end)
 
         if success then
-            break -- stop once we reconnect successfully
+            break
         else
             warn("Reconnect failed, retrying... (" .. tostring(err) .. ")")
         end
     end
 end
-
--- Trigger when error/disconnect occurs
 GuiService.ErrorMessageChanged:Connect(function()
     task.wait(2)
     reconnect()
 end)
+
 
